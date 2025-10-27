d33b343f156ce4cc51a3d7acea42702b3c788ec0
C:/Program Files/Git/ <lahcenehaid@gmail.com>
2025-10-27 12:31:44 +0100

    Création tableau question

---
+++ script.js
@@ -0,0 +1,95 @@
+    export const projet_quiz = {
+    questions: [
+    {
+      text: "Dans quel film de Disney entend-on la chanson “Libérée, délivrée” ?",
+      options: [
+        "Raiponce",
+        "La Reine Des Neiges",
+        "Encato",
+        "Vaiana",
+      ],
+      correct_answer: "La Reine Des Neiges"
+    },{
+    
+      text: "Qui a interprété la chanson “Shape of You”, sortie en 2017 ?",
+      options: [
+        "Ed Sheeran",
+        "Shawn Mendes",
+        "Harry Styles",
+        "Justin Bieber",
+      ],
+      correct_answer: "Ed Sheeran"
+    },{
+      text: "Quel film a remporté l’Oscar du meilleur film en 2020 ?",
+      options: [
+        "Joker",
+        "1917",
+        "Parasite",
+        "Once Upon a Time... in Hollywood",
+      ],
+      correct_answer: "Parasite"
+    },{
+      text: "Quel groupe britannique est connu pour la chanson “Bohemian Rhapsody?"  ,
+      options: [
+        "Queen",
+        "The Beatles",
+        "Coldplay",
+        "Pink Floyd",
+      ],
+      correct_answer: "Queen"
+    },{
+      text: "Dans quel film entend-on la célèbre réplique “Je suis ton père” ?",
+      options: [
+        "Harry Potter et la Coupe de Feu",
+        "Star Wars : L'Empire contre-attaque",
+        "Le Seigneur Des Anneaux : La Communauté de l'Anneau",
+        "Avengers : Endgame",
+      ],
+      correct_answer: "Star Wars : L'Empire contre-attaque"
+    },{
+      text: "Quel artiste a chanté “Like a Prayer?”", 
+      options: [
+        "Madonna",
+        "Lady Gaga",
+        "Beyoncé",
+        "Whitney Houston",
+      ],
+      correct_answer: "Madonna"
+    },{
+      text: "Quel compositeur a créé la musique du film “Inception” ?",
+      options: [
+       "John Williams",
+       "Hans Zimmer",
+       "Ennio Morricone",
+       "James Newton Howard",
+      ],
+      correct_answer: "Hans Zimmer"
+    },{
+      text: "Quel film musical met en scène le groupe ABBA ?",
+      options: [
+       "Moulin Rouge",
+       "Mamma Mia!",
+       "Grease",
+       "La La Land",
+      ],
+      correct_answer: "Mamma Mia!"
+    },{
+      text: "Qui chante “Skyfall”, le thème du film de James Bond du même nom ?",
+      options: [
+       "Adele",
+       "Rihanna",
+       "Sam Smith",
+       "Billie Eilish",
+      ],
+      correct_answer: "Adele"
+    },{
+      text: "Quel film d’animation met en avant la passion de la musique mexicaine et la fête des morts ?",
+      options: [
+       "Coco",
+       "Encanto",
+       "Luca",
+       "Le livre de la Vie",
+      ],
+      correct_answer: "Coco"
+    },]
+}
