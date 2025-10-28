944d46756e4758357ee2e85cd0febd4919c949b6
Hgo92 <hugo_monier@yahoo.fr>
2025-10-28 11:07:07 +0100

    ajout currentQuestionIndex dans game.js

--- game.js
+++ game.js
@@ -1,18 +1,20 @@
 import { projet_quiz } from './script.js';
 
+let currentQuestionIndex = 0;
+
 const question = document.getElementById("question-text");
 const reponses = document.getElementById("options-container");
 const suivant = document.getElementsByClassName("next-button");
 const compteur = document.getElementsByClassName("compteur");
 
-const firstQuestion = projet_quiz.questions[0];
+const firstQuestion = projet_quiz.questions[currentQuestionIndex];
 
 question.innerText = firstQuestion.text;
 
 firstQuestion.options.forEach(option => {
     const option_btn = document.createElement('button');
     option_btn.innerText = option;
-    option_btn.classList.add("option-bouton")
+    option_btn.classList.add("option")
     reponses.appendChild(option_btn);
 
 console.log(option_btn)
