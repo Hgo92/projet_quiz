835d700dc9c401457a979e6bd771aeec96a8c799
Hgo92 <hugo_monier@yahoo.fr>
2025-10-28 11:23:07 +0100

    Ajout des commentaires pour expliquer le code

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
 import { projet_quiz } from './script.js';
 
-let currentQuestionIndex = 0;
+let currentQuestionIndex = 0; // Je prépare l'index des questions, comme ça je pourrais faire +1 à chaque changement de question
 
 const question = document.getElementById("question-text");
 const reponses = document.getElementById("options-container");
-const suivant = document.getElementsByClassName("next-button");
+const suivant = document.getElementById("next-button");
 const compteur = document.getElementsByClassName("compteur");
 
-const firstQuestion = projet_quiz.questions[currentQuestionIndex];
+// J'ai créé des variables et je les ai liées aux éléments du HTML dont j'ai besoin (le bloc question, le bloc réponses, le bouton suivant, le compteur)
 
-question.innerText = firstQuestion.text;
+const firstQuestion = projet_quiz.questions[currentQuestionIndex]; // Ici je vais chercher mes questions et mes réponses dans mon fichier script.js
+
+function loadQuestion() { // Je crée ma fonction qui va charger les questions 
+reponses.innerHTML = " "; // Je vide la section réponses pour être sûr qu'il n'y a pas déjà des boutons
+
+question.innerText = firstQuestion.text; // Je mets le texte de la question dans le bloc question
 
 firstQuestion.options.forEach(option => {
     const option_btn = document.createElement('button');
@@ -17,6 +22,15 @@ firstQuestion.options.forEach(option => {
     option_btn.classList.add("option")
     reponses.appendChild(option_btn);
 
-console.log(option_btn)
-} )
+} ) // Je crée une boucle pour chaque réponse possible (4) : je crée un bouton(createElement), je lui donne le texte de la réponse(.innerText), je lui mets la classe "option" pour que le CSS s'applique (classList.add) et je la mets en enfant du bloc de réponses dans le HTML (appendChild)
+
+}
+
+suivant.addEventListener("click", () => {
+    
+})
+
+// Je crée un écouteur d'événement sur mon bouton suivant (addEventListener)
+
+loadQuestion()
 
